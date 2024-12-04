5a30dccc6be74cb534b2606a3db73d63fe702180
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-04 17:46:13 -0300

    Add multiple new posts with structured data and tags to enhance the content of the Posts array

--- src/app/lib/Posts.ts
+++ src/app/lib/Posts.ts
@@ -27,12 +27,43 @@ export const POSTS_BY_TAG: Record<Tag, TagProps> = {
 
 export const POSTS = [
   {
+    id: 4,
+    title: 'Titulo del articulo',
+    date: 'Dec 3 - 2024',
+    tags: [POSTS_BY_TAG.REACT, POSTS_BY_TAG.ZUSTAND],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iure voluptatum blanditiis doloribus laborum cum aperiam, magnam quasi ratione, inventore totam quae odit voluptatibus fuga. Nostrum distinctio ipsam ducimus animi!',
+    href: 'react-zustand-ts',
+    new: true,
+  },
+  {
+    id: 3,
+    title: 'Titulo del articulo',
+    date: 'Dec 3 - 2024',
+    tags: [POSTS_BY_TAG.REACT, POSTS_BY_TAG.ZUSTAND],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iure voluptatum blanditiis doloribus laborum cum aperiam, magnam quasi ratione, inventore totam quae odit voluptatibus fuga. Nostrum distinctio ipsam ducimus animi!',
+    href: 'react-zustand-ts',
+    new: false,
+  },
+  {
+    id: 2,
+    title: 'Titulo del articulo',
+    date: 'Dec 3 - 2024',
+    tags: [POSTS_BY_TAG.REACT, POSTS_BY_TAG.ZUSTAND],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iure voluptatum blanditiis doloribus laborum cum aperiam, magnam quasi ratione, inventore totam quae odit voluptatibus fuga. Nostrum distinctio ipsam ducimus animi!',
+    href: 'react-zustand-ts',
+    new: false,
+  },
+  {
     id: 1,
     title: 'Titulo del articulo',
     date: 'Dec 3 - 2024',
     tags: [POSTS_BY_TAG.REACT, POSTS_BY_TAG.ZUSTAND],
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus iure voluptatum blanditiis doloribus laborum cum aperiam, magnam quasi ratione, inventore totam quae odit voluptatibus fuga. Nostrum distinctio ipsam ducimus animi!',
-    href: '/',
+    href: 'react-zustand',
+    new: false,
   },
 ];
