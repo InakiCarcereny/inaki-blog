71dffa6ebb5319ec00e1acc58bd4b5a87a70d63d
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-04 17:46:22 -0300

    Enhance Tailwind configuration with new keyframes and animations; unify string quotes for consistency throughout the config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      keyframes: {
+        borderAnimation: {
+          '0%': { borderColor: '#ffffff10a' },
+          '25%': { borderColor: '#ffffff5a' },
+          '50%': { borderColor: '#ffffff4a' },
+          '75%': { borderColor: '#ffffff3a' },
+          '100%': { borderColor: '#ffffff2a' },
+        },
+        rotateBorder: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        border: 'borderAnimation 3s infinite',
+        rotate: 'rotateBorder 3s linear infinite',
       },
     },
   },
