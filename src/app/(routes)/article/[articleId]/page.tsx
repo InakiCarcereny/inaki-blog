c9c39a27f233672444a2f78b3bd24bd1e469a9f8
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-30 15:16:53 -0300

    Enhance ArticlePage with navigation and date icons: add back link and date display improvements for better user interaction and design

--- src/app/(routes)/article/[articleId]/page.tsx
+++ src/app/(routes)/article/[articleId]/page.tsx
@@ -1,4 +1,7 @@
+import { ArrowLeft } from '@/app/icons/ArrowLeft';
+import { Calendar } from '@/app/icons/Calendar';
 import { POSTS } from '@/app/lib/Posts';
+import Link from 'next/link';
 
 export default function ArticlePage({
   params,
@@ -7,17 +10,24 @@ export default function ArticlePage({
 }) {
   const { articleId } = params;
 
-  console.log(articleId);
-
   const filteredPosts = POSTS.filter((post) => post.href === articleId);
 
   return (
     <section className="max-w-[800px] mx-auto w-full h-full flex flex-col mt-10">
       {filteredPosts.map((post) => (
         <article key={post.id} className="flex flex-col gap-6">
+          <Link
+            href="/"
+            className="flex items-center gap-4 text-[#a1a1a1] text-sm hover:underline hover:underline-offset-1 hover:text-white"
+          >
+            <ArrowLeft />
+            Go back
+          </Link>
+
           <header className="flex items-center justify-between w-full">
             <h1 className="text-white text-3xl font-bold">{post.title}</h1>
-            <span className="text-[#a1a1a1] font-semibold text-sm">
+            <span className="text-[#a1a1a1] font-semibold text-sm flex items-center gap-2">
+              <Calendar className="w-5 h-5" />
               {post.date}
             </span>
           </header>
@@ -29,11 +39,11 @@ export default function ArticlePage({
             {post.description}
           </p>
 
-          <img
+          {/* <img
             src={post.image}
             alt={post.title}
             className="rounded-xl w-auto h-full"
-          />
+          /> */}
         </article>
       ))}
     </section>
